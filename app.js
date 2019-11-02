9286f47c031942ce5b4e3161d29d7c5ff819c27c
Kristn Vaughn <vaughn.kristin.n@gmail.com>
2019-11-02 18:26:28 -0400

    connected database and added show route

--- app.js
+++ app.js
@@ -1,49 +1,120 @@
-var express = require('express');
-var app = express();
+var express     = require('express'),
+    app         = express(),
+    bodyParser  = require('body-parser'),
+    mongoose    = require('mongoose');
+
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
-var recipes = [
-    {title: 'Banana Bread', rating: 4, image: 'https://cdn.pixabay.com/photo/2014/11/03/09/39/quark-bread-514890_960_720.jpg', difficulty: "easy", time: "1 hr 30 min", tags: ["Baking"]},
-    {title: 'Bread and Butter Pickles', rating: 5, image: 'https://cdn.pixabay.com/photo/2016/07/15/22/49/pickled-cucumbers-1520638_960_720.jpg', difficulty: "easy", time: "2 hr", tags: ["Canning"]},
-    {title: 'Kidney Bean Salad', rating: 5, image: 'https://cdn.pixabay.com/photo/2016/06/29/09/06/kidney-bean-1486298_960_720.jpg', difficulty: "easy", time: "30 min", tags: ["Side Dish"]},
-    {title: 'Apple Pie Filling', rating: 3, image: 'https://cdn.pixabay.com/photo/2015/01/12/10/44/cake-597175_960_720.jpg', difficulty: "easy", time: "2 hr", tags: ["Canning", "Baking"]}
-];
+// Connect to Database
+mongoose.connect("mongodb://localhost:27017/simplefarmandgarden", {useUnifiedTopology: true, useNewUrlParser: true});
+
+// Schema setup
+var recipeSchema = new mongoose.Schema({
+    title: String,
+    rating: Number,
+    image: String,
+    difficulty: String,
+    time: String,
+    tags: Array,
+    ingredients: Array,
+    directions: String
+});
+
+// Model setup
+var Recipe = mongoose.model("Recipe", recipeSchema);
+
+// var relish = new Recipe({
+//     title: 'Relish',
+//     image: 'https://cdn.pixabay.com/photo/2015/02/06/15/50/canning-626204_960_720.jpg',
+//     difficulty: "easy",
+//     time: "2 hr",
+//     tags: ["Canning"]
+// });
+// relish.save(function(err, recipe){
+//     if(err) {
+//         console.log("Error");
+//     } else {
+//         console.log("Success! Newly created: ");
+//         console.log(recipe);
+//     }
+// });
+
+// RESTFUL ROUTES - a way of mapping HTTP routes and CRUD (Create, Read, Update, Destroy)
+//
+// name        url                  verb        description
+// ==============================================================================================
+// INDEX     /recipes               GET        get all recipes
+// NEW       /recipes/new           GET        displays form to create new recipe
+// CREATE    /recipes               POST       add new recipe to the database
+// SHOW      /recipes/:id           GET        shows info about one recipe
+// EDIT      /recipes/:id/edit      GET        displays edit form to edit a recipe
+// UPDATE    /recipes/:id           PUT        updates a specific recipe and redirects somewhere
+// DESTROY   /recipes/:id           DELETE     deletes a specific recipe and redirects somewhere    
+// ==============================================================================================
 
 app.get('/', function(req, res){
     res.render('home');
 });
 
+// INDEX
 app.get('/recipes', function(req, res){
-    res.render('recipes', {recipes: recipes});
+    // Get recipes from database
+    Recipe.find({}, function(err, recipes) {
+        if(err) {
+            console.log(err);
+        } else {
+            // Render the recipes page and pass through the recipes from database
+            res.render('index', {recipes: recipes});
+        }
+    });
 });
 
+// CREATE
 app.post('/recipes', function(req, res){
     var recipeTitle = req.body.recipetitle;
     var imageURL = req.body.imageurl;
     var recipeDifficulty = req.body.difficulty;
     var recipeTime = req.body.time;
     var recipeTags = req.body.tags;
-    recipes.push({
+    var recipeIngredients = req.body.ingredients;
+    var recipeDirections = req.body.directions;
+    var newRecipe = {
         title: recipeTitle, 
         rating: 0,
         image: imageURL,
         difficulty: recipeDifficulty,
         time: recipeTime,
-        tags: recipeTags
+        tags: recipeTags,
+        ingredients: recipeIngredients.split("\n"),
+        directions: recipeDirections
+    }
+    // Create new recipe and save to database
+    Recipe.create(newRecipe, function(err, recipe) {
+        if(err) {
+            console.log(err)
+        } else {
+            res.redirect('/recipes');
+        }
     });
-    res.redirect('recipes');
 });
 
+// NEW
 app.get('/recipes/new', function(req, res) {
     res.render('new');
 });
 
-app.get('/recipes/:recipe', function(req, res){
-    var recipe = req.params.recipe;
-    res.render('recipe', {recipeName: recipe});
+// SHOW
+app.get('/recipes/:id', function(req, res){
+    Recipe.findById(req.params.id, function(err, recipe) {
+        if(err) {
+            console.log(err);
+        } else {
+            // Render the show page and pass through the specific recipe from database
+            res.render('show', {recipe: recipe});
+        }
+    });
 });
 
 app.listen(3000, function(){
